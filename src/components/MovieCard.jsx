1f6ddec567ea1bb8ce8801dea235e9766ad86f58
Robby94gtr <robertogramazio94@gmail.com>
2025-06-05 19:55:18 +0200

    [UPD] CORS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
-function MovieCard({ title, author, image, review }) {
+function MovieCard({ image, release_year, genre }) {
   return (
     <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center max-w-xs">
       <img
-        src={image}
-        alt={title}
+        src={image || "https://via.placeholder.com/160x240?text=No+Image"}
+        alt={title || "Movie poster"}
         className="w-40 h-60 object-cover rounded mb-4"
       />
       <h2 className="text-xl font-bold mb-2">{title}</h2>
-      <p className="text-gray-700 text-center">{author}</p>
-      <p className="text-gray-700 text-center">{review}</p>
+      <p className="text-gray-700 text-center">{genre}</p>
+      <p className="text-gray-700 text-center">{release_year}</p>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
-import movie from '../assets/movie';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
 
 function HomePage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3001/api/movies')
+      .then(response => {
+        setMovies(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the movies!", error);
+      }
+      );
+  }, []);
+
   return (
-    <>
-      <div className="container d-flex wrap justify-content-center gap-4 p-4">
-        {movie.map((movie) => {
-          const { id, title, image, review } = movie;
-          return (
-            <Link key={id} className="flex col-4 column" to={`/movie/${id}`}>
-              <div className="bg-white rounded-lg hcard shadow-md p-6 flex flex-col items-center max-w-xs">
-                <img
-                  src={image}
-                  alt={title}
-                  className="w-40 h-60 object-cover rounded mb-4"
-                />
-                <h2 className="text-xl font-bold mb-2">{title}</h2>
-                <p className="text-gray-700 text-center">{review}</p>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </>
+    <div className="container d-flex flex-wrap justify-content-center gap-4 p-4">
+      {movies.map((movie) => (
+        <Link key={movie.id} to={`/movie/${movie.id}`} className="text-decoration-none">
+          <MovieCard
+            title={movie.title}
+            genre={movie.genre}
+            image={movie.image}
+            release_year={movie.release_year}
+          />
+        </Link>
+      ))}
+    </div>
   );
 }
 
