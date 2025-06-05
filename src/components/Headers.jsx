0b3d8dc504cbc2cb762eb476bf63e8e874a53386
Robby94gtr <robertogramazio94@gmail.com>
2025-06-05 19:34:59 +0200

    [UPD] Implemented CSS

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,10 +1,12 @@
-function Headers() {
+function Headers({ headers }) {
   return (
-    <div className="flex flex-col gap-2">    
-      <div className="flex items-center gap-2">
-        <span className="font-bold">Questo è il titolo</span>
-        <span>ciao</span>
-      </div>     
+    <div className="flex flex-col gap-2">
+      {headers.map((header, index) => (
+        <div key={index} className="flex items-center gap-2">
+          <span className="font-bold">{header.name}:</span>
+          <span>{header.value}</span>
+        </div>
+      ))}
     </div>
   );
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Headers from '../components/Headers';
+import './DefaultLayout.css'
 
 const headersData = [
   { name: 'App', value: 'My Application' },
@@ -8,15 +9,15 @@ const headersData = [
 
 function DefaultLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="bg-gray-800 text-white p-4">
-        <Headers />
+    <div className="flex flex-col min-h-screen vh vw">
+      <header> 
+        <Headers headers={headersData} />
       </header>
       <main className="flex-grow p-4">
         <Outlet /> {/* Qui verranno renderizzate le pagine */}
       </main>
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        My Application
+      <footer>
+        My Application Footer
       </footer>
     </div>
   );
